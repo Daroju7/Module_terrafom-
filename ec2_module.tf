067eee5dc896ac97dfca533d9cd35461c7cbdf91
daroju7 <darroju7@gmail.com>
2022-11-03 19:25:04 -0400

    am the one

--- ec2_module.tf
+++ ec2_module.tf
@@ -2,7 +2,7 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
 
-  name = "single-instance"
+  name = "Justin7"
 
   ami                    = "ami-0022f774911c1d690"
   instance_type          = "t2.micro"
